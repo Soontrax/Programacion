109cf1cc1f3998dd70e7bfe427b51d1cdc1e5217
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-13 21:43:29 +0200

    Add files via upload

---
+++ Practica01/Practica03.py
@@ -0,0 +1,7 @@
+print "Pon un numero para saber si un numero es par o impar"
+numero=int(raw_input())
+if numero % 2 ==0:
+    print "El numero es par"
+if numero % 2 ==1:
+    print "El numero es impar"
+           
