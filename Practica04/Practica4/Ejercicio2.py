e71ec34f5e9d7e910e33b0247f3bf1c83a46212a
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-19 20:58:15 +0200

    Delete Ejercicio2.py

--- Practica04/Practica4/Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-a = input("Escribe el primer numero:")
-b = input("Pon el segundo numero mayor que: %d \n" % a)
-for z in range(a,b+1):
-    if z%2==0:
-        print "El numero" ,z , "es par"
-    else:
-        print "El numero" ,z , "es impar"
