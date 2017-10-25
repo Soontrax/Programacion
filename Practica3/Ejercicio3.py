e92a1e2402cf44320e8469c2c9733db5457e1819
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-25 13:33:19 +0200

    Add files via upload

---
+++ Practica3/Ejercicio3.py
@@ -0,0 +1,5 @@
+print "Conversor de pies y pulgads a centimetros"
+a = float(input("Introduce una unidad de pies:"))
+x = float(input("Introduce una unidad de pulgadas:"))
+z = (a*12+x)*2.54
+print a , "Pies y", x , "pulgadas son" , z , "cm."
