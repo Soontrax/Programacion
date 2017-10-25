916711ef95f774157a909ae17211aa86cf4ff077
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-25 13:32:10 +0200

    Delete Ejercicio8.py

--- Practica 3/Ejercicio8.py
+++
@@ -1,6 +0,0 @@
-print "Conversor de gruesas y docenas"
-e = input("Escribe una cantidad (entera):")
-i = e/144
-w = (e%144)/12
-x = e%12
-print e , "son" , i , "gruesas," , w , "docenas y" , x , "unidades"
