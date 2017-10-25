fa8854a10e16f266d1929264fe953e1d3312fa25
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-25 13:59:14 +0200

    Add files via upload

---
+++ Practica3/Ejercicio1.py
@@ -0,0 +1,5 @@
+print "Calculo de la media aritmetica"
+z = float(input("Escribe un numero:"))
+x = float(input("Escribe el segundo numero:"))
+w = (z+x)/2
+print "La media aritmetica de" , z , "y de" , x , "es de" ,w
---
+++ Practica3/Ejercicio2.py
@@ -0,0 +1,5 @@
+print "Calculo de indice de masa corporal(imc)"
+x = float(input("¿Cuanto pesas?:"))
+z = float(input("¿Cuanto mides?:"))
+c = z/x**2
+print "Tu(imc)es de:" , c 
