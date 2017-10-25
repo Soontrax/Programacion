4cf6d53095bcebcde98a8f7f756c6727e5843f70
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-25 13:32:25 +0200

    Delete Ejercicio7.py

--- Practica 3/Ejercicio7.py
+++
@@ -1,6 +0,0 @@
-print "Conversor de segundos a horas y minutos"
-x = input("Escribe una cantidad de segundos:")
-a = x/3600
-b = x%3600
-c = b%60
-print x , "Segundos son" , a , "horas," , b , "minutos y" , c , "segundos"
