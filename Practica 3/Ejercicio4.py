f0a81ffdafbb00087adaf1d155263fbdae4004cf
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-25 13:21:20 +0200

    Add files via upload

---
+++ Practica 3/Ejercicio4.py
@@ -0,0 +1,3 @@
+print "Programa para convertir de grados Celsius a Fahrenheit"
+n = float(input("Escribe la temperatura en grados Celsius:"))
+print (1.8*n)+32
---
+++ Practica 3/Ejercicio6.py
@@ -0,0 +1,5 @@
+print "Conversor de segundos a minutos"
+z = input("Escribe una cantidad de segundos:")
+a = z/60
+b = z%60
+print z , "Segundos son" , a , "minutos y" , b , "segundos"
