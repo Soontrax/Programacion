f046b61ac125a49f12642e579fc8530e8c1ab450
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-25 13:32:40 +0200

    Delete Ejercicio5.py

--- Practica 3/Ejercicio5.py
+++
@@ -1,4 +0,0 @@
-print "Conversor de grados Fahrenheit a Celsius"
-x = float(input("Escribe la temperatura en grados Fahrenheit:"))
-c = (x-32)/1.8
-print "La temperatura en Fahrenheit de" , x ,"en grados celsius es de:" ,c
