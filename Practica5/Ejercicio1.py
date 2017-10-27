d657bccfc860f7b99b6f598ffab4b46a99ecfab8
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-10-27 17:37:43 +0200

    Add files via upload

---
+++ Practica5/Ejercicio1.py
@@ -0,0 +1,6 @@
+z = raw_input("Escribe una palabra:")
+w = []
+while z <>"":
+    w = w+[z]
+    z = raw_input("Escribe otra palabra:")
+print w
---
+++ Practica5/Ejercicio2.py
@@ -0,0 +1,6 @@
+z = input("Escribe un numero:")
+w = []
+while z <>"":
+    w = w+[z]
+    z = input("Escribe otro numero:")
+print w
