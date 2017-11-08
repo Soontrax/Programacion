7bb5351c64038d3e244150c093334e0576f2f9f5
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-11-08 20:26:27 +0100

    Add files via upload

---
+++ Practica6/Practica8.py
@@ -0,0 +1,8 @@
+numero = input("Dime cuantas palabras tiene una lista:")
+w = []
+for i in range (numero):
+    a = raw_input("Dime la palabra %d: \n" %(i+1))
+    w = w+[a]
+print "La lista creada es de:"  ,w
+w.sort()
+print "La lista inversa es de:" ,w
