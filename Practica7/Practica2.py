eff4c5d9d420cc222389038eea3321e5d7dd2410
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-11-10 17:59:00 +0100

    Add files via upload

---
+++ Practica7/Practica2.py
@@ -0,0 +1,11 @@
+def nombres (primer,segon,tercer):
+    return primer+" "+segon+" "+tercer
+
+n = raw_input("Escribe tu nombre:")
+s = raw_input("Escribe tu primer apellido:")
+p = raw_input("Escribe tu segundo apellido:")
+a = n
+b = s
+c = p
+total = nombres(n,s,p)
+print total
