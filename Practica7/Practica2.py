88764b8164ea7e953d49c76835c2ad48e1fd8294
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-11-11 19:13:49 +0100

    Update Practica2.py

--- Practica7/Practica2.py
+++ Practica7/Practica2.py
@@ -4,8 +4,5 @@ def nombres (primer,segon,tercer):
 n = raw_input("Escribe tu nombre:")
 s = raw_input("Escribe tu primer apellido:")
 p = raw_input("Escribe tu segundo apellido:")
-a = n
-b = s
-c = p
 total = nombres(n,s,p)
 print total
