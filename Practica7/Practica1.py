221093cd0598adeff40fa54b1e390bfa0a944b9c
Soontrax <32308953+Soontrax@users.noreply.github.com>
2017-11-17 16:20:15 +0100

    Add files via upload

---
+++ Practica7/Practica1.py
@@ -0,0 +1,5 @@
+a = raw_input("Escribe algo:")
+def nombre (texto):
+    return texto
+print "Lo que has escrito en minusculas seria asi:" ,a.lower()
+print "Lon que has escrito en mayusculas seri asi" ,a.upper()
